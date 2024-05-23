194e3cab4e97b75113f3913d4ea169e090a93a10
Le Van Tien <lvt030501@gmail.com>
2024-05-24 06:31:38 +0700

    Update: Optimized build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,8 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
+    performance: {
+        maxEntrypointSize: 896000,
+        maxAssetSize: 896000
+    }
 }) 
